b74324debacaf2944235687f897edca3010b1ff2
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-09-30 01:01:25 +0300

    Business katmanı içerisinde MapperExtensions yazıldı.

---
+++ Portfolio.Business/Extensions/MapperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Portfolio.Business.Mappings.AutoMapper;
+
+namespace Portfolio.Business.Extensions
+{
+    public static class MapperExtensions
+    {
+        public static void MapperProfileExpression<T>(this IProfileExpression profile, MapProfileSourceAndDestination[] mapProfileSourceAndDestinations)
+        {
+            // Döngü ile eşleşmeleri sağlayacağız
+            foreach (var mapProfileSourceAndDestination in mapProfileSourceAndDestinations)
+            {
+                if (mapProfileSourceAndDestination.reverse)
+                    profile.CreateMap(mapProfileSourceAndDestination.source, mapProfileSourceAndDestination.destination).ReverseMap();
+                else
+                    profile.CreateMap(mapProfileSourceAndDestination.source, mapProfileSourceAndDestination.destination);
+            }
+        }
+    }
+}
