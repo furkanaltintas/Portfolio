c4255ce39e0a2ab96dfe52a1956fc7e5fbc6d990
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-09-30 01:07:27 +0300

    Web katmanı içerisinde area ve controller isimlerini küçültmek için constraintsler yazıldı.

---
+++ Portfolio.Web/Constraints/LowercaseAreaRouteConvention.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Portfolio.Web.Constraints
+{
+    public class LowercaseAreaRouteConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            foreach (var controller in application.Controllers)
+            {
+                if (controller.RouteValues.ContainsKey("area")) // controller.ControllerType.FullName.Contains("Areas") (diğer yol)
+                {
+                    //controller.ControllerName = controller.ControllerName.ToLower();
+                    controller!.RouteValues["area"] = controller.RouteValues["area"]!.ToLower();
+                }
+            }
+        }
+    }
+}
---
+++ Portfolio.Web/Constraints/LowercaseControllerRouteConvention.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Portfolio.Web.Constraints;
+
+public class LowercaseControllerRouteConvention : IControllerModelConvention
+{
+    public void Apply(ControllerModel controller) =>
+        controller.ControllerName = controller.ControllerName.ToLower();
+}
