1bd75675c4ab4cbddfde37b7e0a2591974724242
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-09-30 01:09:03 +0300

    Web katmanı içerisinde ControllerExtensions ve ServiceExtensions yazıldı.

---
+++ Portfolio.Web/Extensions/ControllerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.Extensions;
+
+public static class ControllerExtensions
+{
+    public static IActionResult ResponseRedirectAction(
+        this Controller controller,
+        Core.Utilities.Results.Abstract.IResult result,
+        string actionName,
+        string? controllerName = null)
+    {
+        if (!result.IsSuccess)
+            return controller.NotFound();
+
+        if (string.IsNullOrWhiteSpace(controllerName))
+            return controller.RedirectToAction(actionName);
+        return controller.RedirectToAction(actionName, controllerName);
+    }
+
+    public static IActionResult ResponseView<T>(
+        this Controller controller,
+        Core.Utilities.Results.Abstract.IDataResult<T> dataResult)
+    {
+        if (!dataResult.IsSuccess)
+            return controller.NotFound();
+        return controller.View(dataResult);
+    }
+
+    public static IActionResult ResponseViewData<T>(
+    this Controller controller,
+    Core.Utilities.Results.Abstract.IDataResult<T> dataResult)
+    {
+        if (!dataResult.IsSuccess)
+            return controller.NotFound();
+        return controller.View(dataResult.Data);
+    }
+
+}
---
+++ Portfolio.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Portfolio.Business.DependencyResolvers.Autofac;
+
+namespace Portfolio.Web.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static ConfigureHostBuilder ConfigureAutofac(this ConfigureHostBuilder host)
+        {
+            host.UseServiceProviderFactory(new AutofacServiceProviderFactory(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinessModule());
+            }));
+
+            return host;
+        }
+    }
+}
