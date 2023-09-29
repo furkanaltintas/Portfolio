5a201e77869795b8155a0b6e33d87972c245ba7f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-09-30 01:00:19 +0300

    Business katmanı içerisinde Constants klasörü oluşturuldu. Generic mesaj dönme sistemi hazırlandı.

---
+++ Portfolio.Business/Constants/MemberInfoTypeNameExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Portfolio.Business.Constants;
+public static class MemberInfoTypeNameExtensions
+{
+    public static string TypeNameExtensions(this MemberInfo memberInfo)
+    {
+        switch (memberInfo.Name)
+        {
+            case "About":
+                return "Hakkında";
+            case "Contact":
+                return "İletişim";
+            case "Introduce":
+                return "Tanıtım";
+            case "Menu":
+                return "Menü";
+            case "Resume":
+                return "Özgeçmiş";
+            case "Skill":
+                return "Yetenek";
+            case "SocialMedia":
+                return "Sosyal Medya";
+            case "Specialization":
+                return "Uzmanlık";
+            case "Testimonial":
+                return "Referans";
+            default:
+                return memberInfo.Name;
+        }
+    }
+}
---
+++ Portfolio.Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Portfolio.Core.Entities.Abstract;
+
+namespace Portfolio.Business.Constants;
+
+public static class Messages<T> where T : IEntity
+{
+    public static class GenericMessage
+    {
+        private static string TypeName => typeof(T).TypeNameExtensions();
+        public static string TAdded = $"{TypeName} eklendi";
+        public static string TUpdated = $"{TypeName} güncellendi";
+        public static string TDeleted = $"{TypeName} silindi";
+        public static string TUnDeleted = $"{TypeName} silinen öğe geri alındı";
+        public static string TNameAlreadyExists = $"Böyle bir kullanıcı sistemde bulunmamaktadır";
+        public static string NoT = $"Böyle bir {TypeName} bulunmamaktadır";
+    }
+}
