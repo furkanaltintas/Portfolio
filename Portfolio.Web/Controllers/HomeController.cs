d36e53226ef340d7f371f670df00941f450c85e4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-09-30 01:04:26 +0300

    Web katmanı içerisinde HomeController eklendi.

--- Portfolio.Web/Controllers/HomeController.cs
+++ Portfolio.Web/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Business.Repositories.Abstract;
 
-namespace Portfolio.Web.Controllers
+namespace Portfolio.Web.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly IService _service;
+
+    public HomeController(IService service)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _service = service;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _service.MenuService.GetAllByIsActiveMenuAsync();
+        return View(result);
     }
 }
