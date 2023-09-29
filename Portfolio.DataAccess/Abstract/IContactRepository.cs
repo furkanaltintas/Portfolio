81a6bd5cf2925167c35b25480569260a8e63e525
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-09-30 00:52:13 +0300

    DataAccess katmanı içerisinde Abstract klasörü oluşturuldu. IAboutRepository ve IContactRepository eklendi. UnitOfWork sistemi için IRepository oluşturuldu.

---
+++ Portfolio.DataAccess/Abstract/IContactRepository.cs
@@ -0,0 +1,9 @@
+﻿using Portfolio.Core.DataAccess.Abstract;
+using Portfolio.Entities.Concrete;
+
+namespace Portfolio.DataAccess.Abstract;
+
+public interface IContactRepository : IEntityRepository<Contact>
+{
+
+}
---
+++ Portfolio.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Portfolio.Core.DataAccess.Abstract;
+using Portfolio.Core.Entities.Abstract;
+
+namespace Portfolio.DataAccess.Abstract
+{
+    public interface IRepository
+    {
+        IEntityRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity, new();
+
+        TRepository GetRepository<TEntity, TRepository>()
+            where TEntity : class, IEntity, new()
+            where TRepository : IEntityRepository<TEntity>;
+
+        Task SaveAsync();
+        void Save();
+    }
+}
