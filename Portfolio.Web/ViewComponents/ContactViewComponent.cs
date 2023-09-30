64c95f6a076ee3ff2c3922beb19a7144acf25a0f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:24:37 +0300

    Web katmanına Contact ve LeftSideBarViewComponent yapısı eklendi. Html tarafları yazıldı.

--- Portfolio.Web/ViewComponents/ContactViewComponent.cs
+++ Portfolio.Web/ViewComponents/ContactViewComponent.cs
@@ -2,22 +2,21 @@
 using Portfolio.Business.Repositories.Abstract;
 using Portfolio.Entities.DTOs;
 
-namespace Portfolio.Web.ViewComponents
+namespace Portfolio.Web.ViewComponents;
+
+public class ContactViewComponent : ViewComponent
 {
-    public class ContactViewComponent : ViewComponent
+    private readonly IService _service;
+
+    public ContactViewComponent(IService service)
     {
-        private readonly IService _service;
+        _service = service;
+    }
 
-        public ContactViewComponent(IService service)
-        {
-            _service = service;
-        }
-
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
-            var hasContact = await _service.ContactService.HasContactAsync();
-            ContactCreateDto contactAddDto = new() { IsItOnAir = hasContact };
-            return View(contactAddDto);
-        }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var hasContact = await _service.ContactService.HasContactAsync();
+        ContactCreateDto contactAddDto = new() { IsItOnAir = hasContact };
+        return View(contactAddDto);
     }
 }
--- Portfolio.Web/ViewComponents/LeftSideBarViewComponent.cs
+++ Portfolio.Web/ViewComponents/LeftSideBarViewComponent.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Business.Repositories.Abstract;
+using Portfolio.Entities.DTOs;
 
 namespace Portfolio.Web.ViewComponents;
 
 public class LeftSideBarViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IService _service;
+
+    public LeftSideBarViewComponent(IService service)
     {
-        return View();
+        _service = service;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var userResult = await _service.UserService.GetUserAsync();
+        var socialMediaResult = await _service.SocialMediaService.GetAllSocialMediaAsync();
+
+
+        UserProfileDto userProfileDto = new UserProfileDto
+        {
+            Address = userResult.Data.Address,
+            Degree = userResult.Data.Degree,
+            Email = userResult.Data.Email,
+            Picture = userResult.Data.Picture,
+            SocialMediaGetAllDto = socialMediaResult.Data
+        };
+        return View(userProfileDto);
     }
 }
