2dc53e2eb78d1d4ff593e521d4dbe3b3df2a7a8d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:22:49 +0300

    Web katmanı içerisine Identity ve Localizations yapıları eklendi.

---
+++ Portfolio.Web/Identity/CustomClaimsPrincipalFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Portfolio.Entities.Concrete;
+using System.Security.Claims;
+
+namespace Portfolio.Web.Identity;
+
+public class CustomClaimsPrincipalFactory : UserClaimsPrincipalFactory<User, Role>
+{
+    public CustomClaimsPrincipalFactory(UserManager<User> userManager, RoleManager<Role> roleManager, IOptions<IdentityOptions> options) : base(userManager, roleManager, options)
+    {
+    }
+
+    protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user)
+    {
+        var identity = await base.GenerateClaimsAsync(user);
+
+        identity.AddClaim(new Claim("Profile", user.Picture));
+
+
+        return identity;
+    }
+}
---
+++ Portfolio.Web/Localizations/LocalizationIdentityErrorDescriber.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Portfolio.Web.Localizations
+{
+    public class LocalizationIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateUserName),
+                Description = $@"{userName} daha önce başka bir kullanıcı tarafından alınmıştır"
+            };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateEmail),
+                Description = $@"{email} e-posta adresi daha önce sisteme kaydolmuştur"
+            };
+        }
+
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordTooShort),
+                Description = $@"Şifre en az 6 karakter olmalıdır"
+            };
+        }
+    }
+}
