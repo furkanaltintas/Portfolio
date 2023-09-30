0f0a1334fdea8d7f203f5560f916974d4fee9dd8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:05:32 +0300

    Core katmanı içerisinde Interception altyapısı yazıldı.

---
+++ Portfolio.Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,40 @@
+﻿using Castle.DynamicProxy;
+
+namespace Portfolio.Core.Utilities.Interceptors;
+
+public abstract class MethodInterception : MethodInterceptionBaseAttribute
+{
+    // invocation: Aslında bizim metodumuz. Çalıştırılmaya çalışılan metot.
+
+    protected virtual void OnBefore(IInvocation invocation) { }
+    protected virtual void OnAfter(IInvocation invocation) { }
+    protected virtual void OnException(IInvocation invocation) { }
+    protected virtual void OnSuccess(IInvocation invocation) { }
+
+    public override void Intercept(IInvocation invocation)
+    {
+        bool isSuccess = true;
+
+        OnBefore(invocation);
+
+        try
+        {
+            invocation.Proceed(); // Operasyonu yani metodu çalıştır anlamına gelir
+        }
+        catch (Exception e)
+        {
+            isSuccess = false;
+            OnException(invocation);
+            throw;
+        }
+        finally
+        {
+            if (isSuccess)
+            {
+                OnSuccess(invocation);
+            }
+        }
+
+        OnAfter(invocation);
+    }
+}
---
+++ Portfolio.Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Castle.DynamicProxy;
+
+namespace Portfolio.Core.Utilities.Interceptors;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+{
+    public int Priority { get; set; } // Çalışma sırası özelliği için
+
+    public virtual void Intercept(IInvocation invocation)
+    {
+
+    }
+}
