360a2169e53bc7d94c4b3c542335bdc5a8457ca2
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:12:12 +0300

    Business katmanı içerisine ValidationRules->FluentValidation->DtoValidators altına SkillCreateDtoValidator yapısı eklendi.

---
+++ Portfolio.Business/ValidationRules/FluentValidation/DtoValidators/SkillCreateDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Portfolio.Entities.DTOs;
+
+namespace Portfolio.Business.ValidationRules.FluentValidation.DtoValidators;
+
+public class SkillCreateDtoValidator : AbstractValidator<SkillCreateDto>
+{
+    public SkillCreateDtoValidator()
+    {
+        RuleFor(s => s.Name)
+            .NotEmpty()
+            .WithMessage("Ad boş olamaz");
+
+        RuleFor(s => s.Name)
+            .MaximumLength(50)
+            .WithMessage("Ad 50 karakterden büyük olamaz");
+
+        RuleFor(s => s.IconName)
+            .NotEmpty()
+            .WithMessage("İkon adı boş olamaz");
+
+        RuleFor(s => s.IconName)
+            .MaximumLength(100)
+            .WithMessage("İkon 100 karakterden büyük olamaz");
+
+        RuleFor(s => s.Point)
+            .InclusiveBetween<SkillCreateDto, short>(0, 100)
+            .WithMessage("0 ile 100 arasında olmak zorunda");
+    }
+}
