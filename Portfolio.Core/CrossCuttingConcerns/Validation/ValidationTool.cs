c156a915a3b48e3dd05a06769c7685f1a066dd48
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:04:55 +0300

    Core katmanı içerisine ValidationTool yapısı yazıldı. Validasyon yapısı için başlangıç

---
+++ Portfolio.Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Portfolio.Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var validationContext = new ValidationContext<object>(entity);
+            var result = validator.Validate(validationContext);
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}
