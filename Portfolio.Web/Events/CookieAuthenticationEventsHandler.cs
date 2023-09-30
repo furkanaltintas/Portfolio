18bddf6bb61ff02c3230e8fe305b62ba8c2f7926
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:20:39 +0300

    Web katmanı içerisine CookieAuthenticationEventsHandler yapısı eklendi. Login yönlendirme işlemi engellenmiş oldu.

---
+++ Portfolio.Web/Events/CookieAuthenticationEventsHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Portfolio.Web.Events;
+
+public class CookieAuthenticationEventsHandler
+{
+    public static CookieAuthenticationEvents GetCookieAuthenticationEvents()
+    {
+        return new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                if (context.Request.Path.StartsWithSegments("/Admin"))
+                    context.Response.Redirect("/");
+                else
+                    context.Response.Redirect(context.RedirectUri);
+                return Task.CompletedTask;
+            }
+        };
+    }
+}
