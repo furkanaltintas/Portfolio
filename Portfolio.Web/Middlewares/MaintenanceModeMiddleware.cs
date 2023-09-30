55d4165d437376cc32f7b7f58364cf0613b4f789
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:23:14 +0300

    Web katmanı içerisinde MaintenanceModeMiddleware yapısı eklendi

---
+++ Portfolio.Web/Middlewares/MaintenanceModeMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace Portfolio.Web.Middlewares
+{
+    public class MaintenanceModeMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly bool _maintenanceMode;
+        private readonly IConfiguration _configuration;
+
+        public MaintenanceModeMiddleware(RequestDelegate next, bool maintenanceMode, IConfiguration configuration)
+        {
+            _next = next;
+            _maintenanceMode = maintenanceMode;
+            _configuration = configuration;
+        }
+
+
+        public async Task Invoke(HttpContext context)
+        {
+            bool maintenanceMode = _configuration.GetValue<bool>("MaintenanceMode");
+
+            // !context.Request.Path.StartsWithSegments("/maintenance") => Mevcut isteğin 'maintenance' yolunda mı olduğunu kontrol ediyoruz.
+            if (maintenanceMode && !context.Request.Path.StartsWithSegments("/maintenance") && !context.User.Identity!.IsAuthenticated)
+            {
+                context.Response.Redirect("/maintenance");
+                return;
+            }
+
+            if (!maintenanceMode && context.Request.Path.StartsWithSegments("/maintenance") && !context.User.Identity!.IsAuthenticated)
+            {
+                context.Response.Redirect("/");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
+
+// Bakım modunu kontrol eder ve yetkisiz kullanıcılara 503 hizmet kullanılamaz yanıtı verir.
