ab646b793522dc26d51fa0ffbf37c212db4cf176
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:22:01 +0300

    Web katmanı içerisine Password ve UserValidator yapısı eklendi.

---
+++ Portfolio.Web/Validations/PasswordValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Portfolio.Entities.Concrete;
+
+namespace Portfolio.Web.Validations;
+
+public class PasswordValidator : IPasswordValidator<User>
+{
+    public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string? password)
+    {
+        var errors = new List<IdentityError>();
+        if (password!.ToLower().Contains(user.UserName!.ToLower())) // Eğer ki içinde böyle bir data var ise
+        {
+            errors.Add(new() { Code = "PasswordContainUserName", Description = "Şifre alanı kullanıcı adı içeremez" });
+        }
+
+        if(password!.ToLower().Contains(user.Email!.ToLower()))
+        {
+            errors.Add(new() { Code = "PasswordContainEmail", Description = "Şifre alanı e-posta adresi içeremez" });
+        }
+
+        if (errors.Any()) // Eğer herhangi bir if içerisine girmişse, errors içerisinde hata var ise
+        {
+            return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+        }
+
+        return Task.FromResult(IdentityResult.Success);
+    }
+}
---
+++ Portfolio.Web/Validations/UserValidator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Portfolio.Entities.Concrete;
+
+namespace Portfolio.Web.Validations;
+
+public class UserValidator : IUserValidator<User>
+{
+    public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user)
+    {
+        var errors = new List<IdentityError>();
+
+        var isDigit = int.TryParse(user.UserName![0].ToString(), out _);
+        // isDigit true olursa kullanıcı adının ilk karakteri sayısal bir değerdir anlamına gelir.
+
+        if (isDigit)
+        {
+            errors.Add(new()
+            {
+                Code = "UserNameContainFirstLetterDigit",
+                Description = "Kullanıcı Adının ilk karakteri sayısal bir karakter içeremez"
+            });
+        }
+
+        if (errors.Any())
+        {
+            return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+        }
+
+        return Task.FromResult(IdentityResult.Success);
+    }
+}
