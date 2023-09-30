2c351374ae50b53648693b8b23a5c15dc5540c34
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:12:54 +0300

    Business katmanı içerisine DependencyResolvers klasörü altına Autofac ve Microsoft diye ayrılıp işlemler yapıldı.

---
+++ Portfolio.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Portfolio.Business.Repositories.Abstract;
+using Portfolio.Business.Repositories.Concrete.Base;
+using Portfolio.Business.Services.Github;
+using Portfolio.Core.Utilities.Interceptors;
+using Portfolio.DataAccess.Abstract;
+using Portfolio.DataAccess.Concrete;
+
+namespace Portfolio.Business.DependencyResolvers.Autofac;
+
+public class AutofacBusinessModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<GithubApiService>().As<IGithubApiService>();
+        builder.RegisterType<Repository>().As<IRepository>();
+        builder.RegisterType<Service>().As<IService>();
+
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+        builder.RegisterAssemblyTypes(assembly)
+            .AsImplementedInterfaces()
+            .EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions()
+            {
+                Selector = new AspectInterceptorSelector()
+            }).SingleInstance();
+    }
+}
---
+++ Portfolio.Business/DependencyResolvers/Microsoft/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Portfolio.Business.DependencyResolvers.Microsoft
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ServiceExtensions(this IServiceCollection services)
+        {
+            services.AddHttpClient();
+
+            services.AddAutoMapper(typeof(Portfolio.Business.AssemblyRefence).Assembly);
+
+            return services;
+
+        }
+    }
+}
