41ed40f87dd0574d21db8203aec341077ca32f5a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:02:15 +0300

    Core katmanı içerisinde Extensions klasörüne ClaimsPrincipalExtensions ve CustomClaimTypes yazıldı.

---
+++ Portfolio.Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+
+namespace Portfolio.Core.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    // ClaimsPrincipal => Mevcut kullanıcımıza karşılık gelir
+    public static List<string>? Claims(this ClaimsPrincipal claimsPrincipal, string claimType)
+    {
+        var result = claimsPrincipal?.FindAll(claimType)?.Select(c => c.Value).ToList();
+        return result;
+    }
+
+
+    public static string? Claim(this ClaimsPrincipal claimsPrincipal, string claimType)
+    {
+        var result = claimsPrincipal?.FindFirst(claimType)?.Value;
+        return result;
+    }
+
+
+    public static List<string>? ClaimRoles(this ClaimsPrincipal claimsPrincipal)
+    {
+        return claimsPrincipal?.Claims(ClaimTypes.Role);
+    }
+}
---
+++ Portfolio.Core/Extensions/CustomClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Portfolio.Core.Extensions;
+
+public static class CustomClaimTypes
+{
+    public const string Profile = nameof(Profile);
+    public const string Degree = nameof(Degree);
+}
