e6d730ee326bae46e5062147ab15bd06fce0477f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 00:07:49 +0300

    Core katmanı içerisinde BusinessRules yapısı eklendi. Yapı genişletildi.

---
+++ Portfolio.Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,31 @@
+﻿using Portfolio.Core.Utilities.Results.Abstract;
+using Portfolio.Core.Utilities.Results.Concrete;
+using Portfolio.Core.Utilities.Results.Concrete.Error;
+
+namespace Portfolio.Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        // İş Kurallarını Çalıştır
+
+        public static Result Run(params IResult[] logics)
+        {
+            var resultError = ResultError(logics);
+            return (resultError.Item1 ? new ErrorResult(resultError.Item2) : null)!;
+        }
+
+        public static DataResult<T> Run<T>(params IResult[] logics)
+        {
+            var resultError = ResultError(logics);
+            return (resultError.Item1 ? new ErrorDataResult<T>(resultError.Item2) : null)!;
+        }
+
+        private static (bool, string) ResultError(params IResult[] logics)
+        {
+            var errorResults = logics.Where(x => !x.IsSuccess).ToList(); // IsSuccess değeri false olanları getirdi
+            var hasError = errorResults.Any(); // errorResult değeri var mı kontrol edildi var ise hasError true oldu
+            var messages = string.Join("\n", errorResults.Select(x => x.Message)); // Join ile birleştirme yapılarak aralara \n ifadesi konuldu
+            return (hasError, messages);
+        }
+    }
+}
