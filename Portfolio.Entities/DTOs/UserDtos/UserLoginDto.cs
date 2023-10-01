901297a43c8768f6a25859511368e1f17e64c5f1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 23:32:47 +0300

    Entities katmanı içerisinde Dtos klasörüde UserLoginDto yapısı değiştirildi

--- Portfolio.Entities/DTOs/UserDtos/UserLoginDto.cs
+++ Portfolio.Entities/DTOs/UserDtos/UserLoginDto.cs
@@ -2,7 +2,7 @@
 
 public class UserLoginDto
 {
-	public string Username { get; set; } = null!;
-	public string Password { get; set; } = null!;
-	public bool RememberMe { get; set; }
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public bool RememberMe { get; set; }
 }
