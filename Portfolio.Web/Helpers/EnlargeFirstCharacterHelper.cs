3ac9d9e2650c0efe6bc226657bd010e59dd6393a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 23:36:31 +0300

    Web katmanı içerisinde bulunan Helpers klasöründe ViewComponent tagı silme ve İlk karakteri büyük harf yapma sistemi yazıldı

---
+++ Portfolio.Web/Helpers/EnlargeFirstCharacterHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Web.Helpers
+{
+    public static class EnlargeFirstCharacterHelper
+    {
+        public static string EnlargeFirstCharacter(string name)
+        {
+            name = name[0].ToString().ToUpper() + name.Substring(1);
+            // name[0].ToString().ToUpper() ilk karakterin harfi büyütüldü
+            // name.Substring(1) ilk karakter silindi
+            return name;
+        }
+    }
+}
---
+++ Portfolio.Web/Helpers/ViewComponentNameRemoveHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Web.Helpers;
+
+public static class ViewComponentNameRemoveHelper
+{
+    private const string VIEW_COMPONENT = "ViewComponent";
+
+    public static string ViewComponentNameRemove(string viewComponentName)
+    {
+        viewComponentName = viewComponentName.Replace(VIEW_COMPONENT, string.Empty);
+        return viewComponentName;
+    }
+}
