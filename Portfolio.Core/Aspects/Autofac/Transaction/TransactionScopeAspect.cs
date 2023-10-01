fc9d9e2b7a2b35896f6592a6807d4e60c79cd34d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 19:16:11 +0300

    Core katmanı içerisinde Transaction Altyapısı yazıldı.

---
+++ Portfolio.Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,25 @@
+﻿using Castle.DynamicProxy;
+using Portfolio.Core.Utilities.Interceptors;
+using System.Transactions;
+
+namespace Portfolio.Core.Aspects.Autofac.Transaction;
+
+public class TransactionScopeAspect : MethodInterception
+{
+    public override void Intercept(IInvocation invocation)
+    {
+        using (TransactionScope transactionScope = new())
+        {
+			try
+			{
+				invocation.Proceed();
+				transactionScope.Complete(); // İşlemi kabul et
+			}
+			catch (Exception e)
+			{
+				transactionScope.Dispose(); // Yapılan işlemleri geri al
+				throw;
+			}
+        }
+    }
+}
