552ce4e2decd36d9414cc138b425120b404b3dc7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 23:37:48 +0300

    IntroducePageViewModel eklendi. IntroduceViewComponent içerisinde kullanıldı

---
+++ Portfolio.Web/ViewModels/IntroducePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Portfolio.Core.Utilities.Results.Abstract;
+using Portfolio.Entities.DTOs;
+
+namespace Portfolio.Web.ViewModels;
+
+public class IntroducePageViewModel
+{
+    public IDataResult<IntroduceGetDto> Result { get; set; } = null!;
+    public short GitHubRepoCount { get; set; }
+    public bool IsProjectActive { get; set; } // Proje aktif mi
+}
