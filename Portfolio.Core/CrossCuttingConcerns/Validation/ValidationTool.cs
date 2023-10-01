97857eacd54c7b6edfca354b288ce015c7c69287
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 21:29:10 +0300

    Core katmanı içerisindeki ValidationTool da namespace düzeltildi

--- Portfolio.Core/CrossCuttingConcerns/Validation/ValidationTool.cs
+++ Portfolio.Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -1,15 +1,14 @@
 ﻿using FluentValidation;
 
-namespace Portfolio.Core.CrossCuttingConcerns.Validation
+namespace Portfolio.Core.CrossCuttingConcerns.Validation;
+
+public static class ValidationTool
 {
-    public static class ValidationTool
+    public static void Validate(IValidator validator, object entity)
     {
-        public static void Validate(IValidator validator, object entity)
-        {
-            var validationContext = new ValidationContext<object>(entity);
-            var result = validator.Validate(validationContext);
-            if (!result.IsValid)
-                throw new ValidationException(result.Errors);
-        }
+        var validationContext = new ValidationContext<object>(entity);
+        var result = validator.Validate(validationContext);
+        if (!result.IsValid)
+            throw new ValidationException(result.Errors);
     }
 }
