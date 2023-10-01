d08c394516db73cf51138c0dd992b45da772c0cb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2023-10-01 21:30:55 +0300

    Web katmanı içerisinde bulunan CookieAuthenticationEventsHandler yapısının içerisine yorum satırları eklendi. Kodun kullanım amacı ve ne işe yaradığı gibi bilgiler eklendi.

--- Portfolio.Web/Events/CookieAuthenticationEventsHandler.cs
+++ Portfolio.Web/Events/CookieAuthenticationEventsHandler.cs
@@ -8,14 +8,24 @@ public class CookieAuthenticationEventsHandler
     {
         return new CookieAuthenticationEvents
         {
+            // Kullanıcının oturum açma gereksinimini karşılayamadığı durumda nereye yönlendirileceğini belirlemektedir
             OnRedirectToLogin = context =>
             {
+                // context.Request.Path => Eğer istemci tarafından yapılan istek 'Admin' ile başlıyorsa kullanıcıyı ana sayfaya yönlendirecek
                 if (context.Request.Path.StartsWithSegments("/Admin"))
                     context.Response.Redirect("/");
                 else
-                    context.Response.Redirect(context.RedirectUri);
+                    context.Response.Redirect(context.RedirectUri); // Aksi durumda, kullanıcıyı oturum açma sayfasına yönlendirir
                 return Task.CompletedTask;
             }
         };
     }
 }
+
+/*
+ Kodu kullanma amacım
+
+"/Admin" yolu üzerindeki sayfalara giriş yapmaya çalışan kullanıcılara erişimini engellemek için kullandım.
+Admin yolu üzerinde ki bir sayfaya yetkisiz kullanıcı gitmeye çalıştığında AccessDenied yapısı tetiklendiği için Login sayfasına yönlendirmekteydi.
+Login yolunu bulmaması ve giriş yapmaya çalışmaması için bir güvenlik işlemi yapıldı
+ */
